7b857e7f52655d7bf86c64ebd1c0484c0c3e5910
alt-milo <70797933+alt-milo@users.noreply.github.com>
2022-12-22 06:38:54 +0530

    Update Main.js

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -57,12 +57,8 @@ const Main = () => {
             <Education />
             <WorkExperience />
             <Projects />
-            <StartupProject />
             <Achievement />
-            //<Blogs />
-            //<Talks />
             <Twitter />
-            //<Podcast />
             <Profile />
             <Footer />
             <ScrollToTopButton />
