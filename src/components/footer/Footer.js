6b8bf51980f62b1a99fc1c46478f523257851595
alt-milo <70797933+alt-milo@users.noreply.github.com>
2022-12-24 23:17:04 +0530

    added youtube

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ Nirmalya Mukherjee")}
+          {emoji("Made with ❤️ by Nirmalya Mukherjee")}
         </p>
       </div>
     </Fade>
