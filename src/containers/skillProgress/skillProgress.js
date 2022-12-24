7950c52c4df9bc57922ea1d6af8fa17da7dbafa8
alt-milo <70797933+alt-milo@users.noreply.github.com>
2022-12-25 01:25:51 +0530

    Update skillProgress.js

--- src/containers/skillProgress/skillProgress.js
+++ src/containers/skillProgress/skillProgress.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Progress.scss";
 import {illustration, techStack} from "../../portfolio";
 import {Fade} from "react-reveal";
-import Build from "../../assets/lottie/build";
+import data2 from "../../assets/lottie/data2";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 
 export default function StackProgress() {
@@ -29,7 +29,7 @@ export default function StackProgress() {
 
           <div className="skills-image">
             {illustration.animated ? (
-              <DisplayLottie animationData={Build} />
+              <DisplayLottie animationData={data2} />
             ) : (
               <img
                 alt="Skills"
