c5b6594b7e3c84a609195d7e44c5f7f2be84c1ef
alt-milo <70797933+alt-milo@users.noreply.github.com>
2022-12-24 15:36:17 +0530

    footer change

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,7 @@
 import React, {useContext} from "react";
 import "./Footer.scss";
 import {Fade} from "react-reveal";
-//import emoji from "react-easy-emoji";
+import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
@@ -10,7 +10,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Nirmalya Mukherjee portfolio
+          emoji(Made with ❤️ Nirmalya Mukherjee)
         </p>
       </div>
     </Fade>
